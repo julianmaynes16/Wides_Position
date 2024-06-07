ba535be62a418b1df565c25310e4ca0121e34df4
Julian Maynes <80068880+julianmaynes16@users.noreply.github.com>
2024-06-07 02:02:53 -0700

    Added documentation and fixed variable bug from previously using wiringPi

--- get_pos_src/get_pos.cpp
+++ get_pos_src/get_pos.cpp
@@ -13,26 +13,25 @@
 
 int main() {
     gpioInitialise();
+    //establishes pin 17 as input
     gpioSetMode(17, PI_INPUT);
     std::cout << "T265 Pose - Matrix ver." << std::endl;
     std::cout << "Provide pulse to GPIO 17 to begin" << std::endl;
     int value = gpioRead(17);
-    while(value != HIGH){
+    //read returns 1 if pin is HIGH
+    while(value != 1){
       value = gpioRead(17);
     }
     std::cout << "Beginning data collection..." << std::endl;
-    rs2::pipeline pipe;
+    rs2::pipeline pipe;// t265 pipeline declaration
     rs2::config cfg;
     cfg.enable_stream(RS2_STREAM_POSE, RS2_FORMAT_6DOF);
     pipe.start(cfg);
     std::cout << "Pipeline successfully started" << std::endl;
     gpioTerminate();
-    //std::vector<std::array<float, 3>> pos_matrix(2000);
-    //std::vector<std::vector<int>> pos_matrix());
-    //std::vector<float> time_datenum(1000);
-    float** pos_matrix = new float*[LOOP_LIM];
+    float** pos_matrix = new float*[LOOP_LIM]; 
 
-
+    //creates and initializes csv output file
     std::ofstream myFile("pos_result.csv");
     double time_count = 0.0;
     auto start_time = std::chrono::high_resolution_clock::now();
@@ -40,14 +39,12 @@ int main() {
 
     std::cout << "Beginning parsing..." << std::endl;
     for (int n = 0; n < LOOP_LIM; ++n) {
-        
+        //get position and time data 
         float* pos_matrix_item = new float[4];
-
-        //auto start_time = std::chrono::high_resolution_clock::now();
         clock_t start_time = clock();
-
-        auto frames = pipe.wait_for_frames(); // Wait for next frame from camera
-        auto f = frames.first_or_default(RS2_STREAM_POSE); // get frame from pose stream
+        //get data from t265
+        auto frames = pipe.wait_for_frames(); 
+        auto f = frames.first_or_default(RS2_STREAM_POSE); 
         auto pose_data = f.as<rs2::pose_frame>().get_pose_data();
 
         //put values into a matrix
@@ -56,7 +53,7 @@ int main() {
         pos_matrix_item[2] = pose_data.translation.y;
         pos_matrix_item[3] = pose_data.translation.z;
 
-        // Print the x, y, z values of the translation, relative to initial position
+        // Print the x, y, z values of the translation, relative to initial position -- DEBUG PURPOSES
         std::cout << "\r" << "Device Position: " << std::setprecision(4) << std::fixed << pose_data.translation.x << " " <<
         pose_data.translation.y << " " << pose_data.translation.z << " (meters)" << std::endl;
 
@@ -66,28 +63,18 @@ int main() {
         clock_t end_time = clock();
         double elapsed_time = double(end_time - start_time) / CLOCKS_PER_SEC;
         time_count += elapsed_time;
-
-        //std::cout << std::setprecision(3) << "Pos_matrix_item" << pose_data.translation.z << std::endl;
-
+        //puts data into other array
         pos_matrix[n] = pos_matrix_item;
-
-        //delete [] pos_matrix_item;
         
     }
 
     std::cout << "Done! Writing to files..." << std::endl;
-
+    //stores all datapoints in new array
     for(int n = 0; n < LOOP_LIM; n++){ 
         myFile << std::setprecision(4) << pos_matrix[n][0] << "," << pos_matrix[n][1] << "," << pos_matrix[n][2] << "," << pos_matrix[n][3] << ",\n";
-        //std::cout << std::setprecision(3) << "Pos_matrix_item" << pos_matrix[n][1]<< std::endl;
     }
-    //End program
-    std::cout << "Program successfully finished." << std::endl;
 
-    //std::cout << pos_matrix[2][1]<< std::endl;
-   
-    
-    
+    std::cout << "Program successfully finished." << std::endl; 
     //deallocate memory
     for(int n = 0; n < LOOP_LIM; n++){
         delete [] pos_matrix[n];
